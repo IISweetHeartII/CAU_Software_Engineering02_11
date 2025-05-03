9389151e1819223e805a713468cdfee90f5e9d0e
Kim Gyeonghun <gyhub.dev@gmail.com>
2025-05-04 04:38:42 +0900

    Revert "KDH testing... 25.04.10"
    This reverts commit 7f65fe50598870d809806d0a478c69da391055c3.

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * 우트노리 게임의 메인 진입점 클래스입니다.
- */
-public class Main {
-    public static void main(String[] args) {
-        GameEngine game = new GameEngine();
-        game.start();
-    }
-} 
