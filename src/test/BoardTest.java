f725a1aed080594b89b643ff501949b5dbe61928
Kim Gyeonghun <gyhub.dev@gmail.com>
2025-05-05 23:16:09 +0900

    Test: getNNextPosition의 모든 case 테스트 완료 및 성공

--- src/test/BoardTest.java
+++ src/test/BoardTest.java
@@ -43,6 +43,35 @@ public class BoardTest {
         assertNotNull(resultPosition);  // null이 아닌지 확인
         assertEquals(expectedPosition, resultPosition);  // 예상 위치와 일치하는지 확인
 
-        // Todo: center 위치/분기 테스트
+        /// center 분기 테스트 ///
+        // C -> E7
+        startPosition = new Position("C");
+        expectedPosition = new Position("E7");
+        resultPosition = board.getNNextPosition(startPosition, 1);
+        assertNotNull(resultPosition);  // null이 아닌지 확인
+        assertEquals(expectedPosition, resultPosition);  // 예상 위치와 일치하는지 확인
+
+
+        // E2 -> C -> E7
+        startPosition = new Position("E2");
+        expectedPosition = new Position("E7");
+        resultPosition = board.getNNextPosition(startPosition, 1);
+        resultPosition = board.getNNextPosition(resultPosition, 1);
+        assertNotNull(resultPosition);  // null이 아닌지 확인
+        assertEquals(expectedPosition, resultPosition);  // 예상 위치와 일치하는지 확인
+
+        // E2 ->-> E3
+        startPosition = new Position("E2");
+        expectedPosition = new Position("E3");
+        resultPosition = board.getNNextPosition(startPosition, 2);
+        assertNotNull(resultPosition);  // null이 아닌지 확인
+        assertEquals(expectedPosition, resultPosition);  // 예상 위치와 일치하는지 확인
+
+        // E6 ->-> E7
+        startPosition = new Position("E6");
+        expectedPosition = new Position("E7");
+        resultPosition = board.getNNextPosition(startPosition, 2);
+        assertNotNull(resultPosition);  // null이 아닌지 확인
+        assertEquals(expectedPosition, resultPosition);  // 예상 위치와 일치하는지 확인
     }
 }
