9b835e86c1da3a299c5a663dd9048308a460f747
Kim Gyeonghun <gyhub.dev@gmail.com>
2025-05-10 17:57:01 +0900

    feat: 게임 실행을 위한 game loop 추가

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,20 +6,16 @@ import Model.GameModel;
 
 public class Main {
     public static void main(String[] args) {
-        int numPlayers = 2; // Number of players
-        int numPieces = 4; // Number of pieces per player
         // Initialize the game model and view
         /// GameModel gameModel = new GameModel(numPlayers, numPieces);
         /// GameView gameView = new MainUI_Swing(); // interface GameView를 구현한 MainUI_Swing 사용
-
+        GameController gameController;
         // Initialize the game controller
         /// GameController gameController = new GameController(gameModel, gameView);
 
         // Start the game loop
         while (true) {
-            // Handle player input and game logic
-            // For example, you can call gameController.handleRandomThrow() or gameController.handleManualThrow(YutResultType.YUT);
-            // Add your game loop logic here
+            gameController.playGame();
         }
     }
 }
