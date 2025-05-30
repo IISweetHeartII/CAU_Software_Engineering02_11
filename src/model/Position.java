d78413666157cf4339edcc6b630927df172f37bf
Kim Gyeonghun <gyhub.dev@gmail.com>
2025-05-30 22:18:38 +0900

    test: Piece class의 unit test 작성

--- src/model/Position.java
+++ src/model/Position.java
@@ -1,9 +1,7 @@
 package model;
 
 public class Position {
-    // 단순히 String 타입을 사용할 수 있지만,
-    // Position 클래스를 만들어서 타입을 명확히 하고,
-    // 얕은 복사를 제공하기위해 Position 클래스를 사용합니다.
+
     private final String id;
 
     public Position(String id) {
